60d6eed943d1b50b59c16c4cc093809c596a3daf
Brandon Goddard <bgoddard@Brandons-MacBook-Pro.local>
2017-01-22 14:49:32 -0600

    removing test

--- core/constants.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular
-    .module('app.core')
-    .constant('TEST', 'This is a test constant')
-    .constant('moment', moment);
--- core/core.module.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular
-    .module('app.core', [
-        'ui.router', 'ui.router.stateHelper', 'ngMaterial', 'ngAnimate', 'ngResource'
-    ]);
